9186adddc1f464ad1b9e059e5c581628c0b2717b
Donatas <ktp211zdv@yandex.ru>
2025-05-31 11:33:10 +0300

    changed print err

--- services/uniq/cmd/app/app.go
+++ services/uniq/cmd/app/app.go
@@ -14,35 +14,39 @@ func main() {
 
 	err := flagstruct.Configure(&arg)
 	if err != nil {
-		fmt.Println(err)
+		err = fmt.Errorf("error configure cmd line arguments: %v", err.Error())
+		fmt.Println(err.Error())
 		return
 	}
 
 	inFile, err := readfile.CheckInput()
 	if err != nil {
-		fmt.Println(err)
+		err = fmt.Errorf("error input file: %v", err.Error())
+		fmt.Print(err.Error())
 		return
 	}
 	defer inFile.Close()
 
 	outFile, err := readfile.ChechOutput()
 	if err != nil {
-		fmt.Println(err)
+		err = fmt.Errorf("error output file: %v", err.Error())
+		fmt.Print(err.Error())
 		return
 	}
 	defer outFile.Close()
 
 	lines, err := readfile.ReadFile(inFile)
 	if err != nil {
-		fmt.Println(err)
+		err = fmt.Errorf("error read file: %v", err.Error())
+		fmt.Print(err.Error())
 		return
 	}
 
 	uniqLines := uniqstrings.UniqString(lines, arg)
 	_, err = readfile.WriteFile(outFile, uniqLines)
 	if err != nil {
-		fmt.Println(err)
+		err = fmt.Errorf("error write file: %v", err.Error())
+		fmt.Print(err.Error())
 		return
 	}
-
 }
